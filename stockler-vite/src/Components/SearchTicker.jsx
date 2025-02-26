8519f0fc024a4bee11616af7b26089969c7363c1
Joldd <julien.coutouly@gmail.com>
2025-02-26 12:25:01 +0100

    onsearch navigate

--- stockler-vite/src/Components/SearchTicker.jsx
+++ stockler-vite/src/Components/SearchTicker.jsx
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import { SearchBar } from './SearchBar';
 import { SearchContext } from './SearchContext';
+import { useNavigate } from 'react-router-dom';
 
 export function SearchTicker() {
   const { search, setSearch, onSearch } = useContext(SearchContext);
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    onSearch();
+    navigate('/stocks');
+  };
 
   return (
-    <SearchBar search={search} setSearch={setSearch} onSearch={onSearch} />
+    <SearchBar search={search} setSearch={setSearch} onSearch={handleSearch} />
   );
 }
